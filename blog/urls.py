2752332151e3c295261380c7461f6ba2c1dc2855
Lee Dawon <dawon337@gmail.com>
2022-12-27 22:03:00 +0900

    do_it_django_atoz : blog : views.py : function index -> index.html Post.objects.all : models.py Post object copied to blog/views.py index function it goes to index.html Post object __str__ function called

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 이부분을 채울 겁니다!
+    path('',views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    #query to database
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts,
+        }
+    )
