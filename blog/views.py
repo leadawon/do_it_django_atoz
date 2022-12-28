74ff65b18aaf4b9fb321426f05a052069c1b0c15
Lee Dawon <dawon337@gmail.com>
2022-12-28 21:37:04 +0900

    do_it_django_atoz : blog : single_post_page.html views.py -> FBV

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pk')
     #query to database
+    # post 를 가져와라.
 
     return render(
         request,
         'blog/index.html',
         {
             'posts':posts,
+        }
+    )
+
+def single_post_page(request,pk):
+    post = Post.objects.get(pk=pk)
+    # query to database
+    # pk에 해당하는 post 를 가져와라.
+
+    return render(
+        request,
+        'blog/single_post_page.html', #post record를 html파일에 담아서 랜더링한다.
+        {
+            'post':post,
         }
     )
