55bd18d0aff8171f19b565a606010246f069a02d
Lee Dawon <dawon337@gmail.com>
2022-12-31 22:10:34 +0900

    do_it_django_atoz : single_pages : html, urls->views

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(
+        request,
+        'single_pages/landing.html'
+    )
+#blog\views.py 처럼 딕셔너리로 post를 받을 필요가 없다.
+
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
