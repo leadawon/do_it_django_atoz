9805d36fd52914f9369957f942d85a1834ff9e50
Lee Dawon <dawon337@gmail.com>
2023-01-16 20:42:23 +0900

    Update : post_list.html : only staff or superadmin can create Post

--- blog/tests.py
+++ blog/tests.py
@@ -10,6 +10,9 @@ class TestView(TestCase):
         self.user_trump = User.objects.create_user(username='trump', password='somepassword')
         self.user_obama = User.objects.create_user(username='obama', password='somepassword')
 
+        self.user_obama.is_staff = True
+        self.user_obama.save()
+
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
 
@@ -180,9 +183,13 @@ class TestView(TestCase):
         self.assertNotEqual(response.status_code, 200)
 
         self.client.login(username='trump',password='somepassword')
-
         response = self.client.get('/blog/create_post/')
-        self.assertEqual(response.status_code,200)
+        self.assertNotEqual(response.status_code,200)
+
+        self.client.login(username='obama', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.assertEqual('Create Post - Blog',soup.title.text)
@@ -196,8 +203,7 @@ class TestView(TestCase):
                 'content': 'Post Form 페이지를 만듭시다.',
             }
         )
-        self.assertEqual(Post.objects.count(),4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
 
