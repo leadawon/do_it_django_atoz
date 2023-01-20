790decfdccd8dacea2da25a098adc39f30266203
Lee Dawon <dawon337@gmail.com>
2023-01-20 20:24:43 +0900

    Update : files : content Markdown applying

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import os
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 # Create your models here.
 
 class Tag(models.Model):
@@ -32,7 +34,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length = 30)
     hook_text = models.CharField(max_length=100, blank = True)
-    content = models.TextField()
+    content = MarkdownxField()
 
     head_image = models.ImageField(upload_to = 'blog/images/%Y/%m/%d/',blank = True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -56,6 +58,9 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+    def get_content_markdonw(self):
+        return markdown(self.content)
 
 
 
