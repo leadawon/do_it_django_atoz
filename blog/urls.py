7dd8842d0b13f09c548883d90a5af492c7ff7ed8
Lee Dawon <dawon337@gmail.com>
2023-01-27 20:46:30 +0900

    Update base.html search util

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search/<str:q>/',views.PostSearch.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('category/<str:slug>/',views.category_page),
     path('',views.PostList.as_view()),
@@ -10,5 +11,5 @@ urlpatterns = [
     path('create_post/',views.PostCreate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
-path('delete_comment/<int:pk>/', views.delete_comment),
+    path('delete_comment/<int:pk>/', views.delete_comment),
 ]
